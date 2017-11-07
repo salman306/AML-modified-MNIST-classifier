f6cb2e4fa453d48d9d1508f9bc244d21a9d6f6df
Steven Tir <steven.tir@mail.mcgill.ca>
2017-11-07 03:54:18 +0000

    fixes bugs

--- base.py
+++ base.py
@@ -1,6 +1,7 @@
 import keras
 import pandas as pd
 import numpy as np
+from nnconvmodels import NNConvModels
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from keras.models import Sequential
@@ -11,13 +12,13 @@ from keras.utils import np_utils
 
 BATCH_SIZE = 128
 EPOCHS = 60
-VALIDATION = False
+VALIDATION = True 
 SEED = 8
 
 print "> Reading training sets"
 
 X_train = pd.read_csv("processed_train_x.csv")
-X_train = X.iloc[:,1:]
+X_train = X_train.iloc[:,1:]
 y_train = pd.read_csv("train_y.csv", header = None)
 
 # setup one hot encoder
@@ -26,10 +27,10 @@ encoder.fit(y_train) #transform: encode to label, inverse: get back class
 encodermapping = list(encoder.classes_)
 
 #GET MODEL
-model = NNConvModels('model_salman')
+model = NNConvModels('model_salman').getModel()
 
 if VALIDATION:
-	X_train, X_valid, y_train, y_valid = train_test_split(X_train.as_matrix()/255, y_train.as_matrix(), test_size=0.1, random_state = seed)
+	X_train, X_valid, y_train, y_valid = train_test_split(X_train.as_matrix()/255, y_train.as_matrix(), test_size=0.1, random_state = SEED)
 
 	X_valid = X_valid.reshape(X_valid.shape[0], 64,64,1)
 	X_train = X_train.reshape(X_train.shape[0], 64,64,1)
@@ -81,11 +82,3 @@ else:
 	dftestout.to_csv("submission.csv")
 
 print("> Done")
-
-
-#Validation or not
-
-# FIT TRAIN
-
-
-
--- nnconvmodels.py
+++ nnconvmodels.py
@@ -8,8 +8,13 @@ from keras import optimizers
 class NNConvModels():
 	""" Functions hold the convolutional models """
 
+
 	def __init__(self, model_name):
-		return getattr(self, model_name)()	
+		self.NUM_CLASSES = 40
+		self.model_name = model_name
+	
+	def getModel(self):
+		return getattr(self, self.model_name)()	
 	
 	def model_salman(self):
 		""" Returns model """
@@ -30,6 +35,28 @@ class NNConvModels():
 		model.add(Dense(200, activation='tanh'))
 		model.add(Dropout(0.5))
 		model.add(Dense(200, activation='tanh'))
+		model.add(Dense(self.NUM_CLASSES,activation='sigmoid', use_bias = True))
+
+		model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+		return model
+
+	def model_salman_6(self):
+		model = Sequential()
+
+		model.add(Conv2D(30, (3, 3),activation='relu',input_shape=(64,64,1)))
+		model.add(Conv2D(30, (3, 3),activation='relu',input_shape=(64,64,1)))
+		model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Conv2D(60, (3, 3),activation='relu'))
+		model.add(Conv2D(60, (3, 3),activation='relu'))
+		model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Conv2D(120, (2, 2),activation='relu'))
+		model.add(Conv2D(120, (2, 2),activation='relu'))
+		model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+		model.add(Flatten())
+		model.add(Dense(300, activation='tanh'))
+		model.add(Dropout(0.5))
+		model.add(Dense(300, activation='tanh'))
 		model.add(Dense(len(encodermapping),activation='sigmoid', use_bias = True))
 
 		model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
@@ -51,7 +78,7 @@ class NNConvModels():
 		model.add(Dense(200, activation='tanh'))
 		model.add(Dropout(0.5))
 		model.add(Dense(200, activation='tanh'))
-		model.add(Dense(len(encodermapping),activation='sigmoid', use_bias = True))
+		model.add(Dense(self.NUM_CLASSES,activation='sigmoid', use_bias = True))
 
 		model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 		return model
