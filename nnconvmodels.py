0be6c1b8863a00fb674edb3af5674a84c0993d29
salman306 <salman306@gmail.com>
2017-11-06 23:08:08 -0500

    Add files via upload

--- nnconvmodels.py
+++ nnconvmodels.py
@@ -12,10 +12,10 @@ class NNConvModels():
 	def __init__(self, model_name):
 		self.NUM_CLASSES = 40
 		self.model_name = model_name
-	
+
 	def getModel(self):
-		return getattr(self, self.model_name)()	
-	
+		return getattr(self, self.model_name)()
+
 	def model_salman(self):
 		""" Returns model """
 
@@ -40,7 +40,30 @@ class NNConvModels():
 		model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 		return model
 
-	def model_salman_6(self):
+	def model_7(self):
+		model = Sequential()
+
+		model.add(Conv2D(30, (3, 3),activation='relu',input_shape=(64,64,1)))
+		model.add(Conv2D(30, (3, 3),activation='relu',input_shape=(64,64,1)))
+		model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Conv2D(60, (3, 3),activation='relu'))
+		model.add(Conv2D(60, (3, 3),activation='relu'))
+		model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Conv2D(120, (2, 2),activation='relu'))
+		model.add(Conv2D(120, (2, 2),activation='relu'))
+		model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+		model.add(Flatten())
+		model.add(Dense(200, activation='tanh'))
+		model.add(Dropout(0.5))
+		model.add(Dense(200, activation='tanh'))
+		model.add(Dense(len(encodermapping),activation='sigmoid', use_bias = True))
+
+		model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+		return model
+
+
+	def model_8(self):
 		model = Sequential()
 
 		model.add(Conv2D(30, (3, 3),activation='relu',input_shape=(64,64,1)))
@@ -62,6 +85,35 @@ class NNConvModels():
 		model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 		return model
 
+
+
+	def model_9(self):
+		model = Sequential()
+
+		model.add(Conv2D(30, (3, 3),activation='relu',input_shape=(64,64,1)))
+		model.add(Conv2D(30, (3, 3),activation='relu',input_shape=(64,64,1)))
+		model.add(Conv2D(30, (3, 3),activation='relu',input_shape=(64,64,1)))
+		model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Conv2D(60, (3, 3),activation='relu'))
+		model.add(Conv2D(60, (3, 3),activation='relu'))
+		model.add(Conv2D(60, (3, 3),activation='relu'))
+		model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Conv2D(120, (2, 2),activation='relu'))
+		model.add(Conv2D(120, (2, 2),activation='relu'))
+		model.add(Conv2D(120, (2, 2),activation='relu'))
+		model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+		model.add(Flatten())
+		model.add(Dense(300, activation='tanh'))
+		model.add(Dropout(0.5))
+		model.add(Dense(300, activation='tanh'))
+		model.add(Dense(len(encodermapping),activation='sigmoid', use_bias = True))
+
+		model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+		return model
+
+
+
 	def model_steven_samepad(self):
 		model = Sequential()
 		model.add(Conv2D(20, (3, 3),activation='relu',input_shape=(64,64,1), padding='same'))
@@ -82,4 +134,3 @@ class NNConvModels():
 
 		model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 		return model
-
