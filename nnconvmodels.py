2f3458589a694b6fe2cb56504a1584daf48b8293
Steven Tir <steven.tir@mail.mcgill.ca>
2017-11-10 17:58:04 +0000

    more models

--- nnconvmodels.py
+++ nnconvmodels.py
@@ -446,7 +446,7 @@ class NNConvModels():
 		return model
 
 	def model_top15(self):
-		"""??42 epochs)"""
+		"""94.24 (58 epochs)"""
 		model = Sequential()
                 model.add(Conv2D(28, (5, 5),activation='relu',input_shape=(64,64,1), padding='same'))
                 model.add(Conv2D(28, (5, 5),activation='relu',input_shape=(64,64,1), padding='same'))
@@ -462,6 +462,127 @@ class NNConvModels():
 		model.add(Dropout(0.5))
                 model.add(Flatten())
                 model.add(Dense(200, activation='tanh'))
+		model.add(Dropout(0.75))
+                model.add(Dense(200, activation='tanh'))
+                model.add(Dense(self.NUM_CLASSES,activation='softmax', use_bias = True))
+
+                model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+		
+		return model
+
+	def model_top16(self):
+		"""94.28 (30 epochs)"""
+		model = Sequential()
+                model.add(Conv2D(28, (5, 5),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(Conv2D(28, (5, 5),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Conv2D(42, (5, 5),activation='relu', padding='same'))
+                model.add(Conv2D(42, (5, 5),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Conv2D(96, (5, 5),activation='relu', padding='same'))
+                model.add(Conv2D(96, (5, 5),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Flatten())
+                model.add(Dense(200, activation='tanh'))
+		model.add(Dropout(0.75))
+                model.add(Dense(200, activation='tanh'))
+                model.add(Dense(self.NUM_CLASSES,activation='softmax', use_bias = True))
+
+                model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+		
+		return model
+
+	def model_top17(self):
+		"""? (30 epochs)"""
+		model = Sequential()
+                model.add(Conv2D(28, (5, 5),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(Conv2D(28, (5, 5),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Conv2D(42, (5, 5),activation='relu', padding='same'))
+                model.add(Conv2D(42, (5, 5),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Conv2D(104, (5, 5),activation='relu', padding='same'))
+                model.add(Conv2D(104, (5, 5),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Flatten())
+                model.add(Dense(200, activation='tanh'))
+		model.add(Dropout(0.75))
+                model.add(Dense(200, activation='tanh'))
+                model.add(Dense(self.NUM_CLASSES,activation='softmax', use_bias = True))
+
+                model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+		
+		return model
+
+	def model_top18(self):
+		"""94.4 (60 epochs)"""
+		model = Sequential()
+                model.add(Conv2D(64, (3, 3),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(Conv2D(64, (3, 3),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Conv2D(94, (3, 3),activation='relu', padding='same'))
+                model.add(Conv2D(94, (3, 3),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Conv2D(118, (3, 3),activation='relu', padding='same'))
+                model.add(Conv2D(118, (3, 3),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Flatten())
+                model.add(Dense(200, activation='tanh'))
+		model.add(Dropout(0.75))
+                model.add(Dense(200, activation='tanh'))
+                model.add(Dense(self.NUM_CLASSES,activation='softmax', use_bias = True))
+
+                model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+		
+		return model
+
+	def model_top19(self):
+		"""90 (30 epochs)"""
+		model = Sequential()
+                model.add(Conv2D(28, (3, 3),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(Conv2D(28, (3, 3),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+                model.add(Conv2D(90, (3, 3),activation='relu', padding='same'))
+                model.add(Conv2D(90, (3, 3),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+                model.add(Conv2D(50, (3, 3),activation='relu', padding='same'))
+                model.add(Conv2D(50, (3, 3),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Flatten())
+                model.add(Dense(200, activation='tanh'))
+		model.add(Dropout(0.75))
+                model.add(Dense(200, activation='tanh'))
+                model.add(Dense(self.NUM_CLASSES,activation='softmax', use_bias = True))
+
+                model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+		
+		return model
+
+	def model_top20(self):
+		"""80 (30 epochs)"""
+		model = Sequential()
+                model.add(Conv2D(28, (3, 3),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(Conv2D(28, (3, 3),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+                model.add(Conv2D(44, (3, 3),activation='relu', padding='same', strides=2))
+                model.add(Conv2D(44, (3, 3),activation='relu', padding='same', strides=2))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+                model.add(Conv2D(92, (3, 3),activation='relu', padding='same'))
+                model.add(Conv2D(92, (3, 3),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Flatten())
+                model.add(Dense(2000, activation='tanh'))
 		model.add(Dropout(0.75))
                 model.add(Dense(200, activation='tanh'))
                 model.add(Dense(self.NUM_CLASSES,activation='softmax', use_bias = True))
@@ -507,118 +628,130 @@ class NNConvModels():
 		model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 		return model
 
-	def model_steven_funky(self):
-
+	def model_top21(self):
+		"""94.4 (60 epochs)"""
 		model = Sequential()
-		model.add(Conv2D(20, (3, 3),activation='relu',input_shape=(64,64,1), padding='same'))
-		model.add(Conv2D(111, (3, 3),activation='relu',input_shape=(64,64,1), padding='same'))
-		model.add(MaxPooling2D(pool_size=(2,2)))
-		model.add(Conv2D(33, (3, 3),activation='relu', padding='same'))
-		model.add(Conv2D(77, (3, 3),activation='relu', padding='same'))
-		model.add(MaxPooling2D(pool_size=(2,2)))
-		model.add(Flatten())
-		model.add(Dense(200, activation='tanh'))
-		model.add(Dropout(0.59))
-		model.add(Dense(200, activation='tanh'))
-		model.add(Dense(self.NUM_CLASSES,activation='sigmoid', use_bias = True))
-
-		model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
-		return model
-	
-	def model_s(self):
-		
-		model = Sequential()
-		model.add(Conv2D(20, (5, 5),activation='relu',input_shape=(64,64,1), padding='same'))
-		model.add(MaxPooling2D(pool_size=(2,2)))
-		model.add(Conv2D(60, (5, 5),activation='relu', padding='same'))
-		model.add(Conv2D(60, (5, 5),activation='relu', padding='same'))
-		model.add(MaxPooling2D(pool_size=(2,2)))
-
-		model.add(Flatten())
-		model.add(Dense(500, activation='tanh'))
-		model.add(Dropout(0.59))
-		model.add(Dense(self.NUM_CLASSES,activation='sigmoid', use_bias = True))
-
-		model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
-
-		return model
-
-	def model_x(self):
-		
-		model = Sequential()
-		model.add(Conv2D(20, (5, 5),activation='relu',input_shape=(64,64,1), padding='same'))
-		model.add(MaxPooling2D(pool_size=(2,2)))
-		model.add(Conv2D(50, (5, 5),activation='relu', padding='same'))
-		model.add(MaxPooling2D(pool_size=(2,2)))
-
-		model.add(Flatten())
-		model.add(Dense(500, activation='tanh'))
-		model.add(Dense(self.NUM_CLASSES,activation='sigmoid', use_bias = True))
-
-		model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
-
-		return model
-
-	def model_3(self):
-		
-		model = Sequential()
-		model.add(Conv2D(64, (1, 1),activation='relu',input_shape=(64,64,1), padding='same'))
-		model.add(MaxPooling2D(pool_size=(2,2)))
-		model.add(Conv2D(50, (5, 5),activation='relu', padding='same'))
-		model.add(Conv2D(50, (5, 5),activation='relu', padding='same'))
-		model.add(MaxPooling2D(pool_size=(2,2)))
-		model.add(Conv2D(80, (5, 5),activation='relu', padding='same'))
-
-		model.add(Flatten())
-		model.add(Dense(500, activation='tanh'))
-		model.add(Dropout(0.59))
-		model.add(Dense(200, activation='tanh'))
-		model.add(Dense(self.NUM_CLASSES,activation='sigmoid', use_bias = True))
-
-		model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
-
-		return model
-	
-	def model_vgg(self):
-		""" 30.08% """
-		model = Sequential()
-		# input: 100x100 images with 3 channels -> (100, 100, 3) tensors.
-		# this applies 32 convolution filters of size 3x3 each.
-		model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 1)))
-		model.add(Conv2D(32, (3, 3), activation='relu'))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		model.add(Dropout(0.25))
-
-		model.add(Conv2D(64, (3, 3), activation='relu'))
-		model.add(Conv2D(64, (3, 3), activation='relu'))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		model.add(Dropout(0.25))
-
-		model.add(Flatten())
-		model.add(Dense(256, activation='relu'))
+                model.add(Conv2D(256, (3, 3),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(Conv2D(256, (3, 3),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
 		model.add(Dropout(0.5))
-		model.add(Dense(self.NUM_CLASSES, activation='softmax'))
-
-		sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-		model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-		return model
-	def model_mnist(self):
-		""" 78.84% """
-		model = Sequential()
-
-		model.add(Conv2D(32, (5, 5), activation='relu', input_shape=(64, 64, 1),padding='SAME'))
-		model.add(Conv2D(64, (5, 5), activation='relu',padding='SAME'))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		model.add(Dropout(0.25))
-		model.add(Conv2D(64, (5, 5), activation='relu', padding='SAME'))
-		model.add(Conv2D(128, (5, 5), activation='relu', padding='Same'))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		model.add(Dropout(0.25))
-
-		model.add(Flatten())
-		model.add(Dense(7*7*64, activation='relu'))
+                model.add(Conv2D(80, (3, 3),activation='relu', padding='same'))
+                model.add(Conv2D(80, (3, 3),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
 		model.add(Dropout(0.5))
-		model.add(Dense(self.NUM_CLASSES, activation='softmax'))
+                model.add(Conv2D(36, (5, 5),activation='relu', padding='same'))
+                model.add(Conv2D(36, (5, 5),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Flatten())
+                model.add(Dense(200, activation='tanh'))
+		model.add(Dropout(0.75))
+                model.add(Dense(200, activation='tanh'))
+                model.add(Dense(self.NUM_CLASSES,activation='softmax', use_bias = True))
 
-		model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+                model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+		
+		return model
+
+	def model_top22(self):
+
+		model = Sequential()
+                model.add(Conv2D(28, (72, 72),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(Conv2D(28, (72, 72),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Conv2D(36, (36, 36),activation='relu', padding='same'))
+                model.add(Conv2D(36, (36, 36),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Conv2D(96, (18, 18),activation='relu', padding='same'))
+                model.add(Conv2D(96, (18, 18),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Flatten())
+                model.add(Dense(200, activation='tanh'))
+		model.add(Dropout(0.75))
+                model.add(Dense(200, activation='tanh'))
+                model.add(Dense(self.NUM_CLASSES,activation='softmax', use_bias = True))
+
+                model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+		
+		return model
+
+	def model_top23(self):
+
+		model = Sequential()
+                model.add(Conv2D(28, (72, 72),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(Conv2D(28, (72, 72),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Conv2D(36, (36, 36),activation='relu', padding='same'))
+                model.add(Conv2D(36, (36, 36),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Conv2D(128, (18, 18),activation='relu', padding='same'))
+                model.add(Conv2D(128, (18, 18),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Flatten())
+                model.add(Dense(200, activation='tanh'))
+		model.add(Dropout(0.75))
+                model.add(Dense(200, activation='tanh'))
+                model.add(Dense(self.NUM_CLASSES,activation='softmax', use_bias = True))
+
+                model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+		
+		return model
+
+	def model_top24(self):
+
+		model = Sequential()
+                model.add(Conv2D(28, (72, 72),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(Conv2D(28, (72, 72),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(Conv2D(28, (72, 72),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Conv2D(36, (36, 36),activation='relu', padding='same'))
+                model.add(Conv2D(36, (36, 36),activation='relu', padding='same'))
+                model.add(Conv2D(36, (36, 36),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Conv2D(128, (18, 18),activation='relu', padding='same'))
+                model.add(Conv2D(128, (18, 18),activation='relu', padding='same'))
+                model.add(Conv2D(128, (18, 18),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Flatten())
+                model.add(Dense(200, activation='tanh'))
+		model.add(Dropout(0.75))
+                model.add(Dense(200, activation='tanh'))
+                model.add(Dense(self.NUM_CLASSES,activation='softmax', use_bias = True))
+
+                model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+		
+		return model
+
+	def model_top25(self):
+
+		model = Sequential()
+                model.add(Conv2D(64, (72, 72),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(Conv2D(64, (72, 72),activation='relu',input_shape=(64,64,1), padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Conv2D(128, (36, 36),activation='relu', padding='same'))
+                model.add(Conv2D(128, (36, 36),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Conv2D(256, (18, 18),activation='relu', padding='same'))
+                model.add(Conv2D(256, (18, 18),activation='relu', padding='same'))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+		model.add(Dropout(0.5))
+                model.add(Flatten())
+                model.add(Dense(200, activation='tanh'))
+		model.add(Dropout(0.75))
+                model.add(Dense(200, activation='tanh'))
+                model.add(Dense(self.NUM_CLASSES,activation='softmax', use_bias = True))
+
+                model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+		
 		return model
